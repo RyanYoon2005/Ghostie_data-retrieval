fefa3fceba9ec6ae31c26ee0ae5a0de09c449be4
Taewoong Yoon <121777509+RyanYoon2005@users.noreply.github.com>
2026-05-18 23:19:29 +1000

    Update DataRetrieval.py

--- DataRetrieval.py
+++ DataRetrieval.py
@@ -20,7 +20,7 @@ load_dotenv()
 
 dynamodb = boto3.resource(
     "dynamodb",
-    region_name=os.getenv("DYNAMODB_REGION", "us-east-1"),
+    region_name=os.getenv("DYNAMODB_REGION", "ap-southeast-2"),
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
     aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
@@ -252,7 +252,7 @@ def store(payload: StoreRequest):
 
     logsaved = save_scraped_data(hash_key, business_key, payload)
 
-    print(f"{logsaved}")
+    print(f"Data retreival stored: {logsaved}")
 
     return {
         "status":        "STORED",
